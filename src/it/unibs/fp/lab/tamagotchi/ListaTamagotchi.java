638e161a7d751cff5a87e1d8e9a3b17d4c3b639b
SaraFesta <37024637+SaraFesta@users.noreply.github.com>
2018-04-25 15:42:17 +0000

    Update ListaTamagotchi.java

--- src/it/unibs/fp/lab/tamagotchi/ListaTamagotchi.java
+++ src/it/unibs/fp/lab/tamagotchi/ListaTamagotchi.java
@@ -5,13 +5,19 @@ public class ListaTamagotchi
 {
 	private List<Tamagotchi> listaTamagotchi;
 	public final static String TAMAGOTCHI_IN_LISTA = "#%3d\t%s";
-	
+	/**
+	* @return l'attributo listaTamagotchi al quale non possono essere apportate modifiche.
+	*/
 	public List<Tamagotchi> getListaTamagotchi(){
 		return Collections.unmodifiableList (listaTamagotchi);
 	}
 	public ListaTamagotchi(){
 		this.listaTamagotchi = new Vector<Tamagotchi>();
 		}
+	/**
+	* @param tamagotchiDaAggiungere un nuovo tamagotchi da aggiungere solo se il suo nome
+	*non e' gia' presente nella lista, altrimenti sara' lanciata un'eccezione.
+	*/
 	public void addTamagotchi(Tamagotchi tamagotchiDaAggiungere) throws TamagotchiGiaEsistenteException{
 		for(Tamagotchi tamagotchi : listaTamagotchi){
 			if(tamagotchi.getNome().equalsIgnoreCase(tamagotchiDaAggiungere.getNome())) 
